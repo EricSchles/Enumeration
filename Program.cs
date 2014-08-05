cac93c91a961d3f4bb0c845d2c868c68da19cb36
Eric Schles <ericschles@gmail.com>
2014-08-05 09:07:40 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeration
+{
+    class Program
+    {
+        //enum's are static dictionaries..
+        public enum Days { Monday = 1, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+        static void Main(string[] args)
+        {
+            Days day = Days.Monday;
+            Console.WriteLine((int)day);
+            string[] values = Enum.GetNames(typeof(Days));
+            foreach (string s in values)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
